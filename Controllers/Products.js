3b674da816902a6f76865534af413d42137848dd
F3rr3t-git <j03ljac06@gmail.com>
2020-05-23 22:06:56 +0530

    Some files modified

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -1,34 +1,30 @@
 const Product = require('../Models/Product');
 
-exports.getAddProduct = (req, res, next) =>{
+exports.getAddProduct = (req, res, next) => {
+  res.render('Add-Product', {
+    pageTitle: 'Add Product',
+    path: '/Admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
-    res.render('Add-Product', {
-        pageTitle:'Add Product', 
-        path: '/Admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
-    };
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
-
-exports.postAddProduct =  (req, res, next) =>{
-    
-        const product = new Product(req.body.title);
-        product.save();
-        res.redirect('/');
-     
-     }
-
-exports.getProduct = (req, res, next) =>{
-
-    const products = Product.fetchAll();
+exports.getProduct = (req, res, next) => {
+  Product.fetchAll(products => {
     res.render('Shop', {
-        prods : products, 
-        pageTitle : 'Shop', 
-        path: '/', 
-        hasproducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
- };
+  });
+};
--- Models/Product.js
+++ Models/Product.js
@@ -1,16 +1,37 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product{
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'Data',
+  'Products.json'
+);
 
-    constructor(t){
-        this.title = t;
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
+  });
+};
 
-    save(){
-        products.push(this);
-    }
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-    static fetchAll(){
-        return products;
-    }
-}
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
