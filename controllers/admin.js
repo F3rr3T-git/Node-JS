ea540e0a527a9de0efad90d34194487fa31063e1
F3rr3t-git <j03ljac06@gmail.com>
2020-05-28 19:19:50 +0530

    New file added

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,8 +15,8 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  
-  Product.create({
+
+  req.user.createProduct({
     title: title,
     imageUrl: imageUrl,
     price: price,
@@ -41,17 +41,18 @@ exports.getEditProduct = (req, res, next) => {
   }
   
   const prodId = req.params.productId;
-  
-  Product.findAll({where: {id:prodId}})
+
+  req.user.getProducts({where: {id:prodId}})
   .then(products => {
-    if (!products[0]) {
+    const product = products[0];
+    if (!product) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: products[0]
+      product: product
     });
   })
   .catch(err => {
@@ -67,13 +68,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  Product.findAll({where: {id:prodId}})
-  .then(products => {
-      products[0].title = updatedTitle;
-      products[0].imgaeUrl = updatedImageUrl;
-      products[0].description = updatedDesc;
-      products[0].price =updatedPrice;
-      return products[0].save();
+  Product.findByPk(prodId)
+  .then(product => {
+      product.title = updatedTitle;
+      product.imgaeUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      product.price =updatedPrice;
+      return product.save();
   })
   .then(result => {
     //console.log(result);
@@ -89,7 +90,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
 
-  Product.findAll()
+  req.user.getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
